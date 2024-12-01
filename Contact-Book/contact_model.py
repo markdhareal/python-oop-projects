d62f313900d1bdede438a6217cb9d5ab0372a855
Dharyl <programmingdha@gmail.com>
2024-12-01 17:52:48 +0800

    Add Contact Model

---
+++ Contact-Book/contact_model.py
@@ -0,0 +1,8 @@
+class BookContact:
+    def __init__(self, name,phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+    def __str__(self):
+        return f'Name: {self.name} -> Phone: {self.phone} -> Email: {self.email}'
